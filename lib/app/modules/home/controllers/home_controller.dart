c1bbf40f45f610c89643b82c6dc74bfcb97a3469
mosfeqanik <mosfeqanik01@gmail.com>
2022-12-23 11:41:06 +0600

    medicine model class list and model class added

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,61 @@
 import 'package:get/get.dart';
 
+import '../model/MedicineModelClass.dart';
+
 class HomeController extends GetxController {
   //TODO: Implement HomeController
+  var medicineList = <MedicineModel>[].obs;
 
-  final count = 0.obs;
   @override
   void onInit() {
+    medicineList.add(MedicineModel(
+        medicineName: "Ecospirin 150Mg Tablet 14’S",
+        medicineCompanyName: "ACME Limited",
+        medicinePrice: "7.50",
+        medicineDiscount: "8.43",
+        quantity: "0"));
+    medicineList.add(MedicineModel(
+        medicineName: "Asthalin Inhaler 200Md",
+        medicineCompanyName: "Cipla Limited Respiratory",
+        medicinePrice: "118.26",
+        medicineDiscount: "139.13",
+        quantity: "0"));
+    medicineList.add(MedicineModel(
+        medicineName: "Asthalin Inhaler 200Md",
+        medicineCompanyName: "Cipla Limited Respiratory",
+        medicinePrice: "118.26",
+        medicineDiscount: "139.13",
+        quantity: "0"));
+    medicineList.add(MedicineModel(
+        medicineName: "Ecospirin 150Mg Tablet 14’S",
+        medicineCompanyName: "ACME Limited",
+        medicinePrice: "7.50",
+        medicineDiscount: "8.43",
+        quantity: "0"));
+    medicineList.add(MedicineModel(
+        medicineName: "Ecospirin 150Mg Tablet 14’S",
+        medicineCompanyName: "ACME Limited",
+        medicinePrice: "7.50",
+        medicineDiscount: "8.43",
+        quantity: "0"));
+    medicineList.add(MedicineModel(
+        medicineName: "Asthalin Inhaler 200Md",
+        medicineCompanyName: "Cipla Limited Respiratory",
+        medicinePrice: "118.26",
+        medicineDiscount: "139.13",
+        quantity: "0"));
+    medicineList.add(MedicineModel(
+        medicineName: "Asthalin Inhaler 200Md",
+        medicineCompanyName: "Cipla Limited Respiratory",
+        medicinePrice: "118.26",
+        medicineDiscount: "139.13",
+        quantity: "0"));
+    medicineList.add(MedicineModel(
+        medicineName: "Asthalin Inhaler 200Md",
+        medicineCompanyName: "Cipla Limited Respiratory",
+        medicinePrice: "118.26",
+        medicineDiscount: "139.13",
+        quantity: "0"));
     super.onInit();
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
---
+++ lib/app/modules/home/model/MedicineModelClass.dart
@@ -0,0 +1,14 @@
+class MedicineModel{
+  String? medicineName;
+  String? medicineCompanyName;
+  String? medicinePrice;
+  String? medicineDiscount;
+  String? quantity;
+
+  MedicineModel(
+      {this.medicineName,
+      this.medicineCompanyName,
+      this.medicinePrice,
+      this.medicineDiscount,
+      this.quantity});
+}
