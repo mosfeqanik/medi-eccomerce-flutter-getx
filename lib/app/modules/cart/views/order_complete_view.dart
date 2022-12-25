294188490e006687d47510dd55271482bf818dcc
mosfeqanik <mosfeqanik01@gmail.com>
2022-12-25 13:48:51 +0600

    order complete page added

---
+++ lib/app/modules/cart/views/order_complete_view.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:get/get.dart';
+import 'package:medi_eccomerce_flutter_getx/app/routes/app_pages.dart';
+
+import '../../../config/theme/AppColors.dart';
+import '../../../utils/app_strings.dart';
+import '../../../utils/assets_image_path.dart';
+import '../../Common_components/ButtonWidget.dart';
+import '../Components/OrderConfirmationDetails.dart';
+import '../Components/OrderplaceHeading.dart';
+
+class OrderCompleteView extends GetView {
+  const OrderCompleteView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.shopping_cart_outlined,
+              size: 25.sp,
+            ),
+            color: Colors.black,
+          )
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.h, vertical: 10.w),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 10.h,
+            ),
+            OrderConfirmationColumn(),
+            ButtonRow()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row ButtonRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        ButtonWidget(
+          btnHeight: 17.h,
+          btnwidth: 40.w,
+          btnbgColor: Colors.white30,
+          btntextColor: Colors.black,
+          btnborderColor: Colors.white30,
+          btntext: "Back To Home",
+          btnFunc: () {
+            Get.toNamed(Routes.HOME);
+          },
+        ),
+        ButtonWidget(
+          btnHeight: 17.h,
+          btnwidth: 40.w,
+          btnbgColor: AppColors.primaryColor,
+          btntextColor: Colors.white,
+          btnborderColor: AppColors.primaryColor,
+          btntext: "View Orders",
+          btnFunc: () {
+            print('View Orders');
+          },
+        )
+      ],
+    );
+  }
+
+  Column OrderConfirmationColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset(AssetsImagePath.success_logo),
+        SizedBox(
+          height: 20.h,
+        ),
+        const OrderplaceHeading(
+            headingText: AppStrings.Order_Placed_Successfully),
+        const OrderConfirmationDetails(
+          detailText: AppStrings.Order_Placed_Details,
+        )
+      ],
+    );
+  }
+}
